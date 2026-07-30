0bd952913bec62790d3e3ac87a5a8975cf2e8e1c
Yansong Zhang <916125788@qq.com>
2026-07-30 11:41:41 +0800

    fix(api): decouple stream and execution lifecycles

--- api/core/app/apps/workflow/app_queue_manager.py
+++ api/core/app/apps/workflow/app_queue_manager.py
@@ -1,6 +1,7 @@
 from typing import override
 
 from core.app.apps.base_app_queue_manager import AppQueueManager, PublishFrom
+from core.app.apps.execution_coordinator import AppExecutionState
 from core.app.entities.app_invoke_entities import InvokeFrom
 from core.app.entities.queue_entities import (
     AppQueueEvent,
@@ -9,6 +10,7 @@ from core.app.entities.queue_entities import (
     QueueStopEvent,
     QueueWorkflowFailedEvent,
     QueueWorkflowPartialSuccessEvent,
+    QueueWorkflowPausedEvent,
     QueueWorkflowSucceededEvent,
     WorkflowQueueMessage,
 )
@@ -41,4 +43,6 @@ class WorkflowAppQueueManager(AppQueueManager):
             | QueueWorkflowFailedEvent
             | QueueWorkflowPartialSuccessEvent,
         ):
-            self.stop_listen(execution_terminal=True)
+            self.stop_listen(execution_state=AppExecutionState.TERMINAL)
+        elif isinstance(event, QueueWorkflowPausedEvent):
+            self.stop_listen(execution_state=AppExecutionState.PAUSED)
